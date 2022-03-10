8c6081f09bc9d97b884d9ab476a5712b8379c730
p-glinka <glinkapg@hotmail.com>
2022-03-10 19:26:24 -0300

    2do intento

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
+// constantes que utiliza todo el codigo del paquete que instalamos
+
 const express = require("express");
 const app = express();
 const Port = 9000 || 8080;
 const path = require("path"); 
 const hbs = require("hbs");  
-  
+// traigo la libreria para la conexion
+const mysql = require("mysql2"); 
+
+// Creo la coneccion
+const conexion = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password:"m32hh1100",
+    database: "surnutriclubDB"
+});
+
+// conecto a la base de datos
+
+ conexion.connect((error) =>{
+    if(error) throw error;
+    console.log("conexion a la Base de Datos Exitosa") 
+});  
+
+
 // Middelware : Funciones que realizan una tarea especifica en el Servidor
 app.use(express.json());         
 app.use(express.urlencoded({extended:false}));   
 app.use(express.static (path.join(__dirname,"public")));       
-
-
-
 // configuracion del motor de plantillas        
 app.set("view engine", "hbs");    
 app.set("views", path.join(__dirname,"views")); 
 hbs.registerPartials(path.join(__dirname,"views/partials"));
- 
+
 // Rutas HTTP
-  
+
 app.get("/", (req, res) =>{
     res.render("index", {titulo: "Home"})
-});
+});   
 
 app.get("/formulario", (req, res) =>{
     res.render("formulario", {titulo: "formulario para completar"})
@@ -28,13 +46,34 @@ app.get("/formulario", (req, res) =>{
 
 app.get("/productos", (req, res) =>{
     res.render("productos", {titulo: "Productos"})
-});
+});  
+
+/*res.json({
+        Dato: "recibido"   
+    });*/
+    //Desestructuro las Variables
+
+app.post("/formulario", (req, res) =>{ 
+const { nombre, apellido,mail } = req.body
+    if(nombre =="" || apellido == "" || mail == ""){
+        let validacion = "Faltan datos para registrarte"
+        res.render("formulario", {
+        titulo: "Completa el formulario ", validacion
+    } );
+}else{
+    console.log(nombre);
+    console.log(apellido);
+    console.log(mail);
+    res.render("index",{
+        titulo: "index,"
+    })
+}
+}) 
 
 app.get("/sobrenosotros", (req, res) =>{
     res.render("sobrenosotros", {titulo: "Sobre Nosotros "})
-});
+}); 
 
- 
 
 
 app.listen(Port, () =>{
